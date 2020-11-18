c59148c709ad8cf5de9a5ab136ad26c1cf522ab3
Creative Central <72443203+cadburry6969@users.noreply.github.com>
2020-11-18 15:45:14 +0530

    Update radio.sql

--- cc_radio/radio.sql
+++ cc_radio/radio.sql
@@ -1,3 +1,8 @@
+--weight(not sure about this)
 INSERT INTO `items` (name, label, weight) VALUES
 	('radio','radio',1)
 ;
+
+--limit
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('1gr_roka', 'Roka', 1, 0, 1, 0);
